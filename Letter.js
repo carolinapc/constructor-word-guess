4efd9a3f945bd1aa01432a1a10f009dca1a15c3b
Carolina Cavalcanti <carolinapc@gmail.com>
2019-07-27 22:14:44 -0400

    added possibility of continuing playing after finish a game

--- Letter.js
+++ Letter.js
@@ -2,6 +2,10 @@ function Letter(letter){
     this.letter = letter.toUpperCase().trim();
     this.placeholder = "_";
     this.guessed = false;
+    
+    if (this.letter.trim() === "") {
+        this.guessed = true;
+    }
 
     this.print = () => this.guessed ? this.letter : this.placeholder;
 
--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 var Word = require("./Word.js");
 var inquirer = require("inquirer");
+var words = ["Japan","United States","Brazil","Canada","Saudi Arabia","United Kingdom","Spain","South Africa","Mexico","Italy","Germany","China"];
+var word;
 
-var words = ["apple","orange","pinaple","mango","strawberry","grape","banana"];
+function startGame() {
+    //get a random word
+    word = new Word(words[Math.floor(Math.random() * words.length)]);
 
-//get a random word
-var word = new Word(words[Math.floor(Math.random() * words.length)]);
-console.log("\n===============");
-console.log("WORD GUESS GAME");
-console.log("===============");
-console.log(`${word.displayWord()}\n`);
+    console.log("\n===============");
+    console.log("WORD GUESS GAME");
+    console.log("===============");
+    console.log(`${word.displayWord()}\n`);
+    play();
+}
+
+
 
 function play(){
     inquirer.prompt([
         {
             type: "input",
             name: "guess",
-            message: "Try to guess the fruit! Type a letter:"
+            message: "Try to guess the Country! Type a letter:"
         }
     ]).then(resp => {
         
@@ -24,9 +30,11 @@ function play(){
 
         if(word.guessRemaining <= 0){
             console.log(`You Lost! The word is: ${word.word}\n`);
+            finishGame();
         }
         else if(word.lettersRemaining <= 0){
             console.log("CONGRATULATIONS! You Won!\n");
+            finishGame();
         }
         else {
             play();
@@ -34,6 +42,20 @@ function play(){
 
     });
 }
-play();
+
+var finishGame = () => {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "confirm",
+        message: "Would you like to start a new game?"
+    }]).then(answer => {
+        if (answer.confirm)
+            startGame();
+        else
+            process.exit();
+    })
+}
+
+startGame();
 
 
